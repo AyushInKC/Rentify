23221d96c63b2233e38922bf2540984d4eca99c6
AyushInKC <sirfayush1008@gmail.com>
2025-03-16 22:12:39 +0530

    Added

--- src/main/java/com/DTUHackathon/Executive4/O/Configuration/SecurityConfig.java
+++ src/main/java/com/DTUHackathon/Executive4/O/Configuration/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
                 .cors(withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/users/uploadPhoto/**","/lawyers/uploadPhoto/**").permitAll()
-                        .requestMatchers("/users/login", "/users/signup", "/users/**", "/lawyers/**","/api/video/generate-meeting","/chat").permitAll()
+                        .requestMatchers("/users/login","/lawyers/login","/lawyers/signup", "/users/signup", "/users/**", "/lawyers/**","/api/video/generate-meeting","/chat").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
