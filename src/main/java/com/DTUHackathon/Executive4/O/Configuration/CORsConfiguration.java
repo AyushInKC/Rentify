07ef7dffc3ad12a8ed49df41696f31db04bc7ff4
AyushInKC <sirfayush1008@gmail.com>
2025-03-16 18:23:50 +0530

    Added CORS

--- src/main/java/com/DTUHackathon/Executive4/O/Configuration/CORsConfiguration.java
+++ src/main/java/com/DTUHackathon/Executive4/O/Configuration/CORsConfiguration.java
@@ -3,19 +3,23 @@ package com.DTUHackathon.Executive4.O.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CORsConfiguration {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
+                        .allowedOrigins("https://rentrite-live.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
