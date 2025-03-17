375ce925f0f848eba78d165cbc15928faf04d7c5
AyushInKC <sirfayush1008@gmail.com>
2025-03-17 10:10:51 +0530

    Solved

--- src/main/java/com/DTUHackathon/Executive4/O/Controller/MLController.java
+++ src/main/java/com/DTUHackathon/Executive4/O/Controller/MLController.java
@@ -4,6 +4,8 @@ import com.DTUHackathon.Executive4.O.DTO.MLRequestDTO;
 import com.DTUHackathon.Executive4.O.DTO.MLResponseDTO;
 import com.DTUHackathon.Executive4.O.Service.MLService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,14 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class MLController {
 
     private final MLService mlService;
-
+    private static final Logger logger = LoggerFactory.getLogger(MLController.class);
     public MLController(MLService mlService){
         this.mlService = mlService;
     }
     @PostMapping
     public ResponseEntity<MLResponseDTO> chat(@Valid @RequestBody MLRequestDTO message) {
+        logger.info("Received request: {}", message);
+
         MLResponseDTO response = mlService.getChatResponse(message);
+
+        logger.info("Generated response: {}", response);
         return ResponseEntity.ok(response);
     }
-
 }
